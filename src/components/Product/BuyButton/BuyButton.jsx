3a89064827f6abd4b4e68ffc217c5f40cb394c4b
Niobium14 <67588654+Niobium14@users.noreply.github.com>
2020-07-31 15:52:40 +0300

    Button "заказать"
    Button "заказать" was fixed

--- src/components/Product/BuyButton/BuyButton.jsx
+++ src/components/Product/BuyButton/BuyButton.jsx
@@ -1,73 +1,105 @@
 import React from "react";
+import Modal from "react-modal";
 import "./BuyButton.css";
 import logo from "../../photos/black_logo.png";
 
-class BuyButton extends React.Component {
-  state = { show: false };
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
-  showModal = () => {
-    this.setState({ show: true });
-  };
+export default class BuyButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+    };
 
-  hideModal = () => {
-    this.setState({ show: false });
-  };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
 
   sendTelegramRequest = () => {
     let xhr = new XMLHttpRequest();
-    let url = "https://api.telegram.org/bot1368153163:AAEq5F2xFnbVYYekVqqF6oibH5jeLlDLjds/sendMessage?chat_id=-1001279398311&text="
-    let phone = document.getElementById("phone-tg").value
-    let info = document.getElementById("info-tg").value
-    url += "Телефон: " + phone + "%0a"
-    url+= "Данные: " + info + "%0a"
-    url+= new Date().toJSON().slice(0,10).replace(/-/g,'/')
-    xhr.open('GET', url);
+    let url =
+      "https://api.telegram.org/bot1368153163:AAEq5F2xFnbVYYekVqqF6oibH5jeLlDLjds/sendMessage?chat_id=-1001279398311&text=";
+    let phone = document.getElementById("phone-tg").value;
+    let info = document.getElementById("info-tg").value;
+    url += "Телефон: " + phone + "%0a";
+    url += "Данные: " + info + "%0a";
+    url += new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+    xhr.open("GET", url);
     xhr.send();
-  }
+  };
 
   render() {
     return (
-      <main>
-        <Modal show={this.state.show} handleClose={this.hideModal}>
-          <div className="modal-contents">
-            <div className="logo">
-              <img src={logo} />
+      <div>
+        <button onClick={this.handleOpenModal} className="order">
+          Заказать
+        </button>
+        <Modal
+          isOpen={this.state.showModal}
+          contentLabel="Minimal Modal Example"
+          style={customStyles}
+        >
+          <img src={logo} alt="" className="logo" />
+          <a href="#" className="close" onClick={this.handleCloseModal} />
+          <div className="content">
+            <div className="left">
+              <h3>Ваша корзина</h3>
+              <div className="main">
+                <div className="color">
+                  <h4>Цвет</h4>
+                  <div className="color-picker"></div>
+                </div>
+                <div className="quantity">
+                  <h4>Количество</h4>
+                  <div className="quantity-picker"></div>
+                </div>
+              </div>
             </div>
-            <form action="">
-              <input id="phone-tg" type="text" placeholder="Телефон" />
-              <input id="info-tg" type="email" placeholder="Ваши данные" />
-
-              <a onClick={this.sendTelegramRequest} href="#" className="bg-button">
+            <div className="right">
+              <h3>Ваши данные</h3>
+              <input
+                id="phone-tg"
+                type="text"
+                className="phone"
+                placeholder="Телефон"
+              />
+              <input
+                id="info-tg"
+                type="text"
+                className="name"
+                placeholder="Ваши данные, город, новая почта"
+              />
+              <button
+                onClick={(e) => {
+                  this.sendTelegramRequest(e);
+                  this.handleCloseModal(e);
+                }}
+                className="send"
+              >
                 Отправить
-              </a>
-
-            </form>
+              </button>
+            </div>
           </div>
         </Modal>
-
-        <div class="button_cont" align="center" onClick={this.showModal}>
-          <a class="main_b">Заказать</a>
-        </div>
-      </main>
+      </div>
     );
   }
 }
-
-const Modal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
-
-  return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        {children}
-        <div className="close" onClick={handleClose}>
-          +
-        </div>
-      </section>
-    </div>
-  );
-};
-
-const container = document.createElement("div");
-document.body.appendChild(container);
-export default BuyButton;
