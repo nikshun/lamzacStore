70aa5c45614fde0b95252e9e710643b4cf216b24
Niobium14 <67588654+Niobium14@users.noreply.github.com>
2020-08-10 22:18:44 +0300

    New pictures and settings
    Added new pictures in mobile version of site and also was added new settings for CSS3 files.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import css from "./Main.module.css";
 import CarouselBox from "./CarouselBox/CarouselBox";
-import Header from "./Header/Header";
+import NavBar from "./NavBar/NavBar";
 
 const Main = () => {
   return (
     <div className={css.main} id="main">
-      <Header className={css.header}/>
+      <NavBar className={css.navBar}/>
       <CarouselBox className={css.carousel}/>
     </div>
   );
